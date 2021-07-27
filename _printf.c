6bf457c09dc9336285c32a1afc17a2d46282c383
Michaelndula <michaelndula@gmail.com>
2021-07-27 03:02:33 -0700

    a function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,31 +1,37 @@
 #include "holberton.h"
 
 /**
- * _printf - print string as printf
- * @format: number of arguments
- * Return: void
- **/
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
+ */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	unsigned int i, result;
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 
-	result = 0;
-	va_start(ap, format);
+	if (format == NULL)
+		return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			result += pull_print(format[i + 1], &ap);
-			i++;
-		}
-		else
-		{
-			_putchar(format[i]);
-			result++;
-		}
-	}
-	va_end(ap);
-	return (result);
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
